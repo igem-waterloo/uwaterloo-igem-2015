786ebb1cae11d30ae39fa1702dab7ef70d35994a
mattsmart <mattsmart@hotmail.ca>
2015-07-13 12:02:13 -0400

    Functionalize behaviour, add main segment, variable renaming/cleanup

--- models/tridimensional/results_csv.py
+++ models/tridimensional/results_csv.py
@@ -3,49 +3,74 @@ import csv
 import os
 
 
-# helper function for extracting score data
-def get_scores(filedata):
-    # Given a score file, extract the last element of each row
-    # (expected to be a float) and remove the last 2 chars (a newline)
-    assert len(filedata) == 6
-    return [filedata[i].split(" ")[-1][:-1] for i in xrange(4)]
+# constants
+header = ['PAM_1', 'PAM_2', 'PAM_3', 'Tool', 'Init FA', 'Init DNA', 'Final FA', 'Final DNA']
+categories = ['3DNA', 'Chimera']  # tools used to create 64 pam variants
 
 
-# argument parsing
-parser = argparse.ArgumentParser(description='Compile score files into a csv.')
-parser.add_argument('results_dir', metavar='D', type=str, 
-                   help='directory of score files to collect')
-args = parser.parse_args()
-score_file_directory = args.results_dir
-score_files = os.listdir(score_file_directory)  # get list of score files
-
-# csv prep
-header = ['PAM_1','PAM_2','PAM_3','Tool','Init FA','Init DNA','Final FA','Final DNA']
-categories = ['3DNA', 'Chimera']
-csv_dict = {elem:csv.writer(open('%s.csv' % elem,'a'),lineterminator='\n') for elem in categories}
-for elem in categories:
-    filename = '%s.csv' % elem
-    if os.stat(filename).st_size == 0:  # if file empty, write header
-        print "%s is empty, adding header" % filename
-        csv_dict[elem].writerow(header)
-    else:
-        print "%s already exists" % filename
-        
-# append all data to csv
-for i, score_file in enumerate(score_files):
+def get_score_info(score_dir, score_filename):
+    """Extracts pam/tool info and scores as a list from a score file
+    Args:
+        score_dir: path to score file directory
+        score_file: specific score file name (including .txt)
+    Returns:
+        list of scores (as strings)
+    Example:
+        Example score file: 'agg_3DNA.txt'
+            Final FA Score:  2465.219
+            Init DNA Score:   305.504
+            Final FA score:  2044.202
+            Final DNA score: -207.921
+            Time taken:       857.995
+        Expected return: ['a', 'g', 'g', '3DNA', '2465.219', ..., '-207.921']
+    """
     # get textfile classifiers (PAM and category)
-    pam, pam_tool_txt = score_file.split("_")
-    pam_tool = pam_tool_txt.split(".")[0]
+    pam, pam_tool_txt = score_filename.split('_')
+    pam_tool = pam_tool_txt.split('.')[0]
     assert pam_tool in categories
     # read contents of textfile
-    with open(score_file_directory + score_file) as f:
-        filedata = f.readlines()
-    # format row and append to csv
-    scores = get_scores(filedata)
-    row = [pam[0], pam[1], pam[2], pam_tool] + scores
-    csv_dict[pam_tool].writerow(row)
+    with open(score_dir + score_filename) as f:
+        file_lines = f.readlines()
+        assert len(file_lines) == 6
+    # format row
+    scores = [file_lines[i].split(" ")[-1][:-1] for i in xrange(4)]
+    return [pam[0], pam[1], pam[2], pam_tool] + scores
 
-# close csvs
-for elem in categories:
-    with open('%s.csv' % elem) as f: f.close()
-print "csv writing complete"
+
+def results_to_csv(score_file_directory):
+    """Appends data from each result file to an appropriate csv
+    Args:
+        score_file_directory: path to score files
+    Returns:
+        None
+    Notes:
+        - currently creates / looks for a csv for each PAM tool
+    """
+    # csv prep
+    csv_dict = {elem: csv.writer(open('%s.csv' % elem, 'a'), lineterminator='\n') for elem in categories}
+    for elem in categories:
+        filename = '%s.csv' % elem
+        if os.stat(filename).st_size == 0:  # if file empty, write header
+            print "%s is empty, adding header" % filename
+            csv_dict[elem].writerow(header)
+        else:
+            print "%s already exists" % filename
+    # append all data to csv
+    for i, score_filename in enumerate(os.listdir(score_file_directory)):
+        score_info = get_score_info(score_file_directory, score_filename)
+        category = score_info[3]
+        csv_dict[category].writerow(score_info)
+    # close csvs
+    for elem in categories:
+        with open('%s.csv' % elem) as f:
+            f.close()
+    print "csv writing complete"
+
+
+if __name__ == '__main__':
+    # argument parsing
+    parser = argparse.ArgumentParser(description='Compile score files into a csv.')
+    parser.add_argument('results_dir', metavar='D', type=str, help='directory of score files to collect')
+    args = parser.parse_args()
+    # write to csv
+    results_to_csv(args.results_dir)
