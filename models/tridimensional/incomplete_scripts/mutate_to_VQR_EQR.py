59552a0ae23ac3f747ef671da041ce5a3cb2a49d
Mark Lubberts <mark.g.lubberts@gmail.com>
2015-07-26 16:14:25 -0400

    Add difference_test.py script to compare 3DNA and Chimera, update repacking in mutate_pdb to be outside for loop add mutate_to_VQR_EQR.py which generates VQR and EQR Kleinstiver mutants using mutate_pdb.py Add code variants for the different fold tree testing

--- models/tridimensional/incomplete_scripts/mutate_to_VQR_EQR.py
+++ models/tridimensional/incomplete_scripts/mutate_to_VQR_EQR.py
@@ -2,7 +2,7 @@ import os
 
 from rosetta import *
 from toolbox import mutate_residue
-
+rosetta.init()
 
 def mutate_pose(pose, mutations):
     """Applies list of mutations to the given template pose and returns a mutated version
@@ -18,26 +18,23 @@ def mutate_pose(pose, mutations):
     mutant_pose = Pose()
     mutant_pose.assign(pose)
     for aa_num, aa_replacement in mutations:
-        # ensure mutation is valid and apply it
         assert isinstance(aa_num, int)
         assert isinstance(aa_replacement, str) and len(aa_replacement) == 1
+        scorefxn = get_fa_scorefxn()
         mutant_pose = mutate_residue(mutant_pose, aa_num, aa_replacement)
-        # specify a pose packer to repack the mutation region
+    # kims lines from D050 example
+    # =================================
     pose_packer = standard_packer_task(mutant_pose)
     pose_packer.restrict_to_repacking()
-    # =================================
-    # mark's hack segment
-    # =================================
     # This is a hack, but I want to test. Can't set a movemap, resfiles
-    # might be the way to go. Freeze all residues.
+    # might be the way to go. Freeze all residues. 
     pose_packer.temporarily_fix_everything()
     # Let's release the PI domain
     for i in range(1110, 1388):
-        pose_packer.temporarily_set_pack_residue(i, True)
-    # =================================
-    # specify the rotamer mover and apply repacking
-    packmover = PackRotamersMover(get_fa_scorefxn(), pose_packer)
+        pose_packer.temporarily_set_pack_residue(i,True)
+    packmover = PackRotamersMover(scorefxn, pose_packer)
     packmover.apply(mutant_pose)
+    # =================================
     return mutant_pose
 
 
@@ -52,15 +49,24 @@ def mutate_pdb(input_pdb_path, mutations, output_directory, output_id):
     Returns:
         full filepath to the output pdb with the specified mutations
     """
-    assert os.path.isfile(input_pdb_path)
     pose_template = pose_from_pdb(input_pdb_path)
     pose_mutant = mutate_pose(pose_template, mutations)
     if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+        os.makedirs(output_directory)    
     output_pdb_path = os.path.join(output_directory, output_id + ".pdb")
-    pose_mutant.dump(output_pdb_path)
+    pose_mutant.dump_pdb(output_pdb_path)
     return output_pdb_path
 
     
 if __name__ == '__main__':
-    print "main behaviour not yet implemented"
+    '''
+    This script creates the VQR and EQR mutants from the Kleinstiver 2015 paper
+    It is based off the 
+    There are difference in residue numbering between Kleinstiver and the
+    Anders et al. (2014) experiments that produces the PDB via X-ray
+    crystalography. Due to a couple of gaps, and the way PyRosetta handles
+    residue numbering, the offset is not constant either. The mutant locations
+    in this script have been verified using PyMOL.
+    '''
+    mutate_pdb("4UN3.tgg.pdb", [(1135+34,'V'),(1335+23,'Q'),(1337+23,'R')],"mutate_folder",'4UN3.VQR')
+    mutate_pdb("4UN3.tgg.pdb", [(1135+34,'E'),(1335+23,'Q'),(1337+23,'R')],"mutate_folder",'4UN3.EQR')
