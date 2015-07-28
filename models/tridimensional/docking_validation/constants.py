8a5ca0a4b011a934c795b8e3442f0319ddf7f18b
mattsmart <mattsmart@hotmail.ca>
2015-07-28 03:08:05 -0400

    Support for pam length 4, add dock time to score file

--- models/tridimensional/docking_validation/constants.py
+++ models/tridimensional/docking_validation/constants.py
@@ -1,2 +1,8 @@
-DNA_ALPHABET = "actg"
-PAM_TEMPLATE_SEQUENCE = "tggt"
+# PAM constants
+DNA_ALPHABET = "acgt"  # used for iterating over pam variants
+PAM_TEMPLATE_SEQUENCE = "tggt"  # pam sequence for the progenitor file
+
+# IO constants
+SCOREFILE_LINES = 6  # number of lines in score .txt files from dock_variants.py
+CSV_HEADER = ['PAM_1', 'PAM_2', 'PAM_3', 'PAM_4', 'Tool', 'Init FA', 'Final FA', 'Init DNA', 'Final DNA',
+              'Total Time', 'Dock Time']
--- models/tridimensional/docking_validation/dock_variants.py
+++ models/tridimensional/docking_validation/dock_variants.py
@@ -6,29 +6,32 @@ from time import time
 
 from rosetta import *
 
+from constants import SCOREFILE_LINES
 from results_csv import results_to_csv
+from utility import pam_string_from_int
 
 
 # variations to account for
-dna_nts = "acgt"
 programs = ["Chimera", "3DNA"]
 
 
-def write_dock_stats(score_directory, filename, dock_stats, time_diff):
+def write_dock_stats(score_directory, filename, dock_stats, time_diff_total, time_diff_docking):
     """Writes separate files for each variant
     Notes:
     - score_directory has the form results/timestamped folder/
-    - dock stats has the form: [dna_init, dna_final, fa_init, fa_final]
+    - dock stats has the form: [fa_init, fa_final, dna_init, dna_final]
     - 'a' appends in case of accidental duplication
     - scores and times are passed in explicitly for argument clarity
     """
     path = os.path.join(score_directory, filename)
     f = open(path, 'a')
-    f.write("Initial DNA score: %8.3f\n" % dock_stats[0])
-    f.write("Final DNA Score: %8.3f\n" % dock_stats[1])
-    f.write("Initial FA score: %8.3f\n" % dock_stats[2])
-    f.write("Final FA score: %8.3f\n" % dock_stats[3])
-    f.write("Total variant time: %8.3f\n" % time_diff)
+    f.write("Initial FA score: %8.3f\n" % dock_stats[0])
+    f.write("Final FA score: %8.3f\n" % dock_stats[1])
+    f.write("Initial DNA score: %8.3f\n" % dock_stats[2])
+    f.write("Final DNA Score: %8.3f\n" % dock_stats[3])
+    f.write("Total variant time: %8.3f\n" % time_diff_total)
+    f.write("Docking variant time: %8.3f\n" % time_diff_docking)
+    assert len(f.readlines()) == SCOREFILE_LINES
     f.close()
     return
 
@@ -36,34 +39,26 @@ def write_dock_stats(score_directory, filename, dock_stats, time_diff):
 def dock_simple(pose):
     """Coarse docking of a pose representing a PAM / program variant
     Returns:
-        list of scores in the form [dna_init, dna_final, fa_init, fa_final]
+        list of scores in the form [fa_init, fa_final, dna_init, dna_final]
     Notes:
     - docking optimizes for DNA score, which is weighted differently than "Full Atom" (FA) score
-    - dna score from demo/D110_DNA_interface.py
-    Potential Bugs:
-    - setup_foldtree(...) crashes on some systems/configurations
-    - docking.set_partners(...) may not be needed
-    - use default foldtree (foldtree from pdb) for now
     """
     # specify scoring functions
     fa_score = get_fa_scorefxn()
     dna_score = create_score_function('dna')
     dna_score.set_weight(fa_elec, 1)
-
     # specify docking protocol
     docking = DockMCMProtocol()
     docking.set_scorefxn(dna_score)
     docking.set_scorefxn_pack(fa_score)
     docking.set_partners("B_ACD")
-
     # obtain initial and final scores after docking
     dna_init = dna_score(pose)
     fa_init = fa_score(pose)
     docking.apply(pose)
     dna_final = dna_score(pose)
     fa_final = fa_score(pose)
-    
-    return [dna_init, dna_final, fa_init, fa_final]
+    return [fa_init, fa_final, dna_init, dna_final]
 
 
 def dock_complex(pose):
@@ -73,37 +68,45 @@ def dock_complex(pose):
     Notes:
     - not implemented
     """
-    raise exception("Complex docking not implemented")
+    raise Exception("Complex docking not implemented")
 
 
-def dock_variants(pam_variants, path_to_scores, path_to_pdbs='', complex_docking_flag=False):
+def dock_variants(pam_variants, path_to_scores, path_to_pdbs='', complex_docking_flag=False, pam_length=4):
     """Docks and scores 2 pdbs for each PAM variant (one for each nt program) using simple docking
     Args:
         pam_variants: list of integers (any from 0 to 63 without repeats) which map to pam strings
         path_to_scores: path to the subdirectory of "results" where the variants are stored
         path_to_pdbs: [default: current directory] path to location of chimera/3DNA folders of PAM variants
         complex_docking_flag: [default: False] if True, use complex dock function (NOT IMPLEMENTED)
+        pam_length: [default: 4] length of the pam sequence to be investigated
     Notes:
     - creates a text file (e.g. 'results_agg_Chimera.txt') for each variant
     - path to variants is typically root/results/<timestamped folder>/<variants>
     - assumes current directory is the root of a folder that contains pdbs in Chimera and 3DNA directories
     """
-    for i in pam_variants:
-        variant = dna_nts[i / 16] + dna_nts[i / 4 % 4] + dna_nts[i % 4]
+    for idx in pam_variants:
+        variant = pam_string_from_int(idx, pam_length)
         for program in programs:
             print "Running for variant: %s_%s" % (variant, program)
             pdb_path = os.path.join(path_to_pdbs, program, "4UN3." + variant + ".pdb")
+
             # track runtime while loading and passing pose to the simple docker
-            time_init = time()
+            time_init_total = time()
             loaded_pose = pose_from_pdb(pdb_path)
+
+            time_init_docking = time()
             if complex_docking_flag:
-                dock_complex(loaded_pose)
+                dock_stats = dock_complex(loaded_pose)
             else:
                 dock_stats = dock_simple(loaded_pose)
+
             time_final = time()
+            time_diff_total = time_final - time_init_total
+            time_diff_docking = time_final - time_init_docking
+
             # write results to file
             results_filename = variant + "_" + program + ".txt"
-            write_dock_stats(path_to_scores, results_filename, dock_stats, time_final - time_init)
+            write_dock_stats(path_to_scores, results_filename, dock_stats, time_diff_total, time_diff_docking)
             print "Finished writing scores for variant: %s_%s" % (variant, program)
     return
 
@@ -112,9 +115,9 @@ if __name__ == '__main__':
     # create parser and parse arguments
     parser = argparse.ArgumentParser(description='Run docking and scoring on PAM variants')
     parser.add_argument('-s', '--start', metavar='N', type=int,
-                        help='starting PAM number (0 = aaa), inclusive')
+                        help='starting PAM number (0 = aaaa), inclusive')
     parser.add_argument('-e', '--end', metavar='N', type=int,
-                        help='ending PAM number (63 = ttt), inclusive')
+                        help='ending PAM number (255 = tttt), inclusive')
     parser.add_argument('-o', '--output_dir', metavar='D', type=str,
                         help='path to output directory for variant scores')
     parser.add_argument('--pdb_dir', metavar='D', default='', type=str,
@@ -123,10 +126,12 @@ if __name__ == '__main__':
                         type=str, help='[switch] select complex docking (default: "False")')
     parser.add_argument('--csv', metavar='B', nargs='?', const=True, default=False,
                         type=str, help='[switch] compile scores to csv (default: "False")')
+    parser.add_argument('--pam64', metavar='B', nargs='?', const=3, default=4,
+                        type=str, help='[switch] assume 64 pam variants (default: assume 256)')
     args = parser.parse_args()
-    
-    # setup range of pam variants   
-    assert 0 <= args.start <= args.end <= 63
+
+    # setup range of pam variants
+    assert 0 <= args.start <= args.end <= (4 ** args.pam64) - 1
     pam_variants_to_score = range(args.start, args.end + 1)
 
     # setup output path for scoring
@@ -141,11 +146,13 @@ if __name__ == '__main__':
     except OSError as exc:
         if exc.errno == errno.EEXIST and os.path.isdir(path_to_scores):
             pass
-        else: raise
-    
+        else:
+            raise
+
     # initialize pyrosetta and score variants
     init(extra_options="-mute all")  # reduce rosetta print calls
-    dock_variants(pam_variants_to_score, path_to_scores, path_to_pdbs=args.pdb_dir, complex_docking_flag=args.complex)
+    dock_variants(pam_variants_to_score, path_to_scores,
+                  path_to_pdbs=args.pdb_dir, complex_docking_flag=args.complex, pam_length=args.pam64)
 
     # collect score txt files into a csv
     if args.csv:
