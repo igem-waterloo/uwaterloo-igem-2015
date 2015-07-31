5a5ff5f74b1c8811d7324ffbc283b57800c6d610
Salahub <cdsalahub@gmail.com>
2015-07-31 17:59:35 -0400

    PyRosetta Output Analysis Function

---
+++ models/tridimensional/data_kleinstiver/Analysis of PyRosetta Output.py
@@ -0,0 +1,59 @@
+
+import scipy, scipy.stats.kendalltau, scipy.stats.chisquare
+
+def los_clnr(los):
+    """
+    Consumes a list of strings containing newline characters and commas, and 
+    cleans the strings by removing the newline characters and splitting by 
+    commas before returning a list of lists, where each list corresponds to
+    the important data from each string
+    """
+    newline_remove = map(lambda (el): el.strip(), los)
+    comma_split = map(lambda (el): el.split(','), newline_remove)
+    as_ints = [comma_split[0]] + map(lambda (el): map(float, el), comma_split[1:])
+    return as_ints
+
+def sort_data(lst):
+    """
+    Consumes a list produced by reading a csv file with titled columns
+    corresponding to PyRosetta outputs
+    """
+    cleaned_data = los_clnr(lst)
+    titles = cleaned_data[0]
+    just_vals = cleaned_data[1:]
+    properly_sorted_lst = []
+    for i in range(len(titles)):
+        i_lst = []
+        for val_lst in just_vals:
+            i_lst.append(val_lst[i])
+        properly_sorted_lst.append(i_lst)
+    sorted_dict = {}
+    for i in range(len(titles)):
+        sorted_dict[titles[i]] = properly_sorted_lst[i]
+    return sorted_dict
+        
+
+def get_tau_n_chi(sorted_data, exp_key):
+    """
+    Consumes a dictionary of results that is output from sort_data
+    and calculates kendall's tau and the chi square statistic value for each
+    key with the specified key as the data representing the expected value
+    """
+    obs_keys = sorted_data.keys().remove(exp_key)
+    chitau_dict = {}
+    exp_array = scipy.array(sorted_data[exp_key])
+    for k in obs_keys:
+        k_dict = {}
+        obs_array = scipy.array(sorted_data[k])
+        chi_test = chisquare(obs_array, f_exp=exp_array)
+        k_dict['tau'] = kendalltau(exp_array, obs_array)
+        k_dict['chi_sq_val'] = chi_test[0]
+        k_dict['chi_sq_p'] = chi_test[1]
+        chitau_dict[k] = k_dict
+    return chitau_dict
+
+def main(filename):
+    data = file(filename, 'r')
+    clean_data = sort_data(data)
+    results = get_tau_n_chi(clean_data)
+    return results
