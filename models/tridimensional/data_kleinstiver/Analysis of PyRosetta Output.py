9e2178d27e6dc34022d98687ac5b8a5de7864a88
Salahub <cdsalahub@gmail.com>
2015-08-10 10:23:01 -0400

    Hopefully the last

--- models/tridimensional/data_kleinstiver/Analysis of PyRosetta Output.py
+++ models/tridimensional/data_kleinstiver/Analysis of PyRosetta Output.py
@@ -1,4 +1,6 @@
-import scipy, scipy.stats.kendalltau, scipy.stats.chisquare
+import scipy
+from scipy.stats import kendalltau
+from scipy.stats import chisquare
 
 
 def csv_parser(los):
@@ -33,14 +35,26 @@ def calc_tau_and_chi(sorted_data, exp_key):
     and calculates kendall's tau and the chi square statistic value for each
     key with the specified key as the data representing the expected value
     """
-    obs_keys = sorted_data.keys().remove(exp_key)
     chitau_dict = {}
-    exp_array = scipy.array(sorted_data[exp_key])
-    for k in obs_keys:
+    bins = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    t_exp_array = scipy.array(sorted_data[exp_key])
+    c_exp_lst = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for val in sorted_data[exp_key]:
+        for i in xrange(1,11):
+            if bins[i-1] < val and bins[i] > val:
+                c_exp_lst[i-1] += 1
+    c_exp_array = scipy.array(c_exp_lst)
+    for k in sorted_data.keys():
         k_dict = {}
-        obs_array = scipy.array(sorted_data[k])
-        chi_test = chisquare(obs_array, f_exp=exp_array)
-        k_dict['tau'] = kendalltau(exp_array, obs_array)
+        t_obs_array = scipy.array(sorted_data[k])
+        c_obs_lst = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for val in sorted_data[k]:
+            for i in xrange(1,11):
+                if bins[i-1] < val and bins[i] > val:
+                    c_obs_lst[i-1] += 1
+        c_obs_array = scipy.array(c_obs_lst)
+        chi_test = chisquare(c_obs_array, f_exp=c_exp_array)
+        k_dict['tau'] = kendalltau(t_exp_array, t_obs_array)[0]
         k_dict['chi_sq_val'] = chi_test[0]
         k_dict['chi_sq_p'] = chi_test[1]
         chitau_dict[k] = k_dict
@@ -64,4 +78,3 @@ def main(dataname, ref_name, filename):
     res_file = file(filename, 'w')
     res_file.writelines(res_lst)
     res_file.close()
-    return results
