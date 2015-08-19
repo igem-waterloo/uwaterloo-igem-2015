bff4e20d09e874cdeb47688a2186ef280a40e687
mattsmart <mattsmart@hotmail.ca>
2015-08-19 00:04:11 -0400

    cleanup genome initializer and prep for all domains

--- models/targeting/init_genome_camv.py
+++ models/targeting/init_genome_camv.py
@@ -2,17 +2,27 @@ from converter_fasta import convert_fasta_to_string
 from genome_classes import Genome, Domain, Target
 
 
-def init_genome_camv():
-    # initialize camv genome, domains, targets
-    dt = 0.001
-    complex_concentration = 135000000000
-
+def init_genome_camv(dt=0.001, complex_concentration=135000000000):
+    # initialize camv genome
     sequence = convert_fasta_to_string("genome_camv.fasta")
     genome_camv = Genome(sequence)
-    # These next two aren't at all accurate yet
-    P6 = Domain("P6", 500, 600, "promoter", genome_camv)
-    P6_1 = Target("P6_1", "ggagaaagaaaagatatttaaaa", "ggagaaagaaaagatatttaaaa", 521, complex_concentration, 1, dt, P6)
 
+    # initialize camv genome domains
+    #ncr_left_of_35S = ...
+    #ncr_right_of_35S = ...
+    #promoter_35S = ...
+    #promoter_19S = ...
+    #gene_P1 = ...
+    #gene_P2 = ...
+    #gene_P3 = ...
+    #gene_P4 = ...
+    #gene_P5 = ...
+    gene_P6 = Domain("P6", 500, 600, "orf", genome_camv)
+
+    # initialize camv genome targets
+    target_P6_1 = Target("P6_1", "ggagaaagaaaagatatttaaaa", "ggagaaagaaaagatatttaaaa", 521, complex_concentration, 1, dt, gene_P6)
+
+    """ isaac's testing block
     print P6_1.grna
     print P6.target_location("P6_1")
     print P6_1.cut_probability
@@ -24,4 +34,6 @@ def init_genome_camv():
     print P6.target_location("P6_1")
     print P6_1.cut_probability
     print P6_1.shift
+    """
+
     return genome_camv
