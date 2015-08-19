0bf6450a94f360946adff1dcb60b8253b14da031
mattsmart <mattsmart@hotmail.ca>
2015-08-19 00:13:43 -0400

    filetype assert and cleanup

--- models/targeting/converter_fasta.py
+++ models/targeting/converter_fasta.py
@@ -1,6 +1,11 @@
-def convert_fasta_to_string(file_name):
-    # takes a genome FASTA and outputs a string of that genome
-    with open(file_name) as f:
-        # splits by lines, removes first line, joins lines together
-        sequence = ''.join(f.read().split('\n')[1:]).lower()
+def convert_fasta_to_string(filename):
+    """Takes a genome FASTA and outputs a string of that genome
+    Args:
+        filename: fasta file
+    Returns:
+        string of the genome sequence
+    """
+    assert filename.split('.')[-1] == 'fasta'  # assert correct file type
+    with open(filename) as f:
+        sequence = ''.join(f.read().split('\n')[1:]).lower()  # splits by lines, removes first line, joins lines
     return sequence
