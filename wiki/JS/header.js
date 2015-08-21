55611fa5dc9c07050a7e1421be183939551ba7a7
Isaac <isaacellmen@gmail.com>
2015-08-21 19:39:30 -0400

    bug fixes for header

--- wiki/JS/header.js
+++ wiki/JS/header.js
@@ -1,39 +1,43 @@
 // scripts for header
 
+// for capitalizing id
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 // detects scroll for top resizing and lower nav show
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
     $('.navbar-fixed-top').addClass('shrink');
-    $('.navbar-lower').removeClass('hide-lower')
+    if ($('#inner-page-links').children().length > 0) {
+      $('.navbar-lower').removeClass('hide-lower')
+    }
   } else {
     $('.navbar-fixed-top').removeClass('shrink');
     $('.navbar-lower').addClass('hide-lower')
   }
 });
 
-// set lower navbar
-$('.navbar-lower').affix({
-  offset: {top: 200}
-});
-
 // fills in lower nav with inner page links
 $(document).ready(function(){
-$('.link').each(function(i, obj) {
-    $("#inner-page-links").append('<li><a href="#'+obj.id+'">'+obj.id+'</a></li>');
-});});
+  $('.accordion-heading').addClass('link');
+  $('.link').each(function(i, obj) {
+     $("#inner-page-links").append('<li><a href="#" class="scroll-link">'+obj.id.capitalize()+'</a></li>');
+  });
+  $(".scroll-link").each(function(i, obj) {
+    $(obj).click(function() {
+      scrollToAnchor(obj.text.toLowerCase());
+    });
+  });
+  if ($('#inner-page-links').children().length < 1) {
+    $('.navbar-lower').addClass('hide-lower')
+  }
+});
 
 // for smooth scrolling
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
+function scrollToAnchor(aid){
+    var aTag = $('#'+aid);
+    if(aTag.length){  
+      $('html, body').animate({scrollTop:$(aTag).position().top}, 'slow');
     }
-  });
-});
+}
