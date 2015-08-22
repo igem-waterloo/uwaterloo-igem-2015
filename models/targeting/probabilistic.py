3c8a41c1c3be582e6861cdf24ef45f501143c195
Salahub <cdsalahub@gmail.com>
2015-08-22 19:21:44 -0400

    Added improved indel function and fixed up the concentration probability function

--- models/targeting/probabilistic.py
+++ models/targeting/probabilistic.py
@@ -1,5 +1,7 @@
 from numpy import prod
-from random import randint
+from numpy.random import randint
+from numpy.random import choice
+import math
 
 # constants which may be used in other scripts/the simulation itself
 # vector of mismatch values with index relative to distance from PAM (Hsu et al. from MIT)
@@ -7,6 +9,13 @@ mismatch_decay_values = [0.000, 0.000, 0.014, 0.000, 0.000, 0.395, 0.317, 0.000,
                          0.445, 0.508, 0.613, 0.841, 0.732, 0.828, 0.615, 0.804, 0.685, 0.583]
 # average time for SpyCas9 cutting in perfect match case
 average_cut_time = 60.0
+
+# distribution of indel size from CRISPResso
+indel_sizes = range(-20, 9)
+indel_probs = [0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001,
+               0.001, 0.001, 0.003, 0.003, 0.001, 0.023, 0.001, 0.005, 0.007, 
+               0.016, 0.01, 0.06, 0.84, 0.01, 0.005, 0.001, 0.001, 0.001,
+               0.001, 0.001, 0.001]
 
 def prob_concentration(concentration, num_mismatches):
     """Computes the concentration factor of the total cut probability
@@ -16,12 +25,18 @@ def prob_concentration(concentration, num_mismatches):
     Returns:
         float in [0.0, 1.0]
     Notes:
-        - the formulas were generated using multiple regression in R with data from Kuscu et al 2014
+        - the formulas were generated using regression and fitting 
+        in Excel with data from Wu et al 2014
     """
-    if num_mismatches == 0:  # no mismatches case
-        return (10**(-0.9935))*(concentration**0.465691)
-    else:
-        return (10**(0.036775*num_mismatches-1.616842))*(concentration**0.29122)
+    glob_scale = 2/math.pi # ensure it's between 0 and 1
+    mismatch_scale = 354.3859 - 15.8762*num_mismatches # generated using regression in Excel
+    return (glob_scale*math.atan(concentration/mismatch_scale))
+    #Holding on to old R regression formulas for now in case the new suggested
+    #formula is not acceptable, and for easy reference
+    #if num_mismatches == 0:  # no mismatches case
+    #    return (10**(-0.9935))*(concentration**0.465691)
+    #else:
+    #    return (10**(0.036775*num_mismatches-1.616842))*(concentration**0.29122)
 
 
 def prob_cut(grna, target, concentration, dt):
@@ -49,7 +64,7 @@ def prob_cut(grna, target, concentration, dt):
           is individually important and better fit by a particular level curve
     """
     assert len(grna) == len(target)
-    prob_factor_time = dt/average_cut_time  # assume uniform distribution in time - TODO change to poisson
+    prob_factor_time = 1 - math.exp(-(average_cut_time)*dt) 
     mismatch_decay_subset = [mismatch_decay_values[idx] for idx in xrange(len(grna)) if grna[idx] != target[idx]]
     prob_factor_mismatch = prod(mismatch_decay_subset)
     prob_factor_concentration = prob_concentration(concentration, len(mismatch_decay_subset))
@@ -68,8 +83,13 @@ def nt_rand(insertion_size):
     return insertion
 
 def indel():
-    del_left = randint(0,4)
-    del_right = randint(0,4)
+    size = choice(indel_sizes, 1, p = indel_probs)
     insert = randint(0,8)
-
+    total_del = abs(size - insert)[0]
+    if not total_del == 0:
+        del_left = randint(math.floor(total_del/4), total_del)
+        del_right = total_del - del_left
+    else:
+        del_left = 0
+        del_right = 0
     return [del_left, del_right, insert]
