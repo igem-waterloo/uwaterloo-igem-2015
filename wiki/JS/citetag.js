645619d7e34777d56b8fcbe4c923494d8c65b6d2
James Hawley <jamesrichardhawley@gmail.com>
2015-09-08 19:22:08 -0400

    Updating and adding citetag to each page
    -added citetag.js and references.json to Waterloo template
    -citetags.js updated to use smooth scrolling from header.js
    -Example page has the MWE from testcitation.html (removed testcitation.html because of it)

--- wiki/JS/citetag.js
+++ wiki/JS/citetag.js
@@ -1,15 +1,35 @@
 // Implements the <cite> tag.  Requires a "ref" attribute that corresponds to one listed in references.json and that
 // references.json be included in the global scope
+
+var refLinks = {};
+
 $(document).ready(function() {
   $( "cite" ).each(
     function( index ) {
-      var refID = $(this).attr("ref");  
-    
+      var refID = $(this).attr("ref");
+
       // Add link with ordinal citation inside <cite> tag
-      $(this).html( "<a href=\"#" + refID + "\"><em>[" + (index + 1) + "]</em></a>");
-    
+      $(this).html( "<a class=\"ref-link\" href=\"#" + refID + "\"><em>[" + (index + 1) + "]</em></a>");
+
+      // Add refID to list
+      scrollLinks[$(this).text()] = refID;
+
       // Add appropriate li to the references list
-      $("#reflist").append("<li id=\"" + refID + "\">" + references[refID] + "</li>")
+      $("#reflist").append("<li id=\"" + refID + "\">" + references[refID] + "</li>");
     }
   );
+
+  $(".ref-link").each(function(i, obj) {
+      $(obj).click(function() {
+          scrollToAnchor(scrollLinks[obj.text]);
+      });
+  });
 });
+
+// for smooth scrolling
+function scrollToAnchor(aid){
+    var aTag = $('#'+aid);
+    if(aTag.length){
+      $('html, body').animate({scrollTop:$(aTag).position().top}, 'slow');
+    }
+}
