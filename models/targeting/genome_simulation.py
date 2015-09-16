46285b1d1b19ea2fe14bd96e63687d6a41700fb2
Isaac <isaacellmen@gmail.com>
2015-09-15 20:04:27 -0400

    Large deletions in simulation

--- models/targeting/genome_simulation.py
+++ models/targeting/genome_simulation.py
@@ -42,10 +42,14 @@ genome_camv.initialize_target_cut_probabilities(dt)
 target_dict = genome_camv.get_targets_from_genome()
 open_targets = genome_camv.get_open_targets_from_genome()
 probability_to_repair = prob_repair(dt)
+double_cut_probability = 0.5#1.55*10.0**(-5)
 
 # for logging data
 data_log = ""
 data_file = os.path.join(data_folder, "simulation_data")
+
+# optional plotting
+make_plot = False
 
 
 for turn in xrange(total_turns):
@@ -57,12 +61,20 @@ for turn in xrange(total_turns):
     targets_from_genome = genome_camv.get_targets_from_genome()
 
     # deletion module
-    #if len(open_targets) > 1:
-        # do deletion permutation probabilities for deletion of pairs based on distance
-        # regenerate targets_from_genome
-        # if large deletion:
-        #   first, second = min(target1,target2), max(target1, target2)
-        #   turn_log += "Large deletion spanning from", first, "to", second + "\n"
+    if len(open_targets) > 1:
+        # time_with_double_cut += dt
+        print open_targets
+        double_cut_success = False
+        if random.random() < double_cut_probability:
+            double_cut_success = True
+        if double_cut_success:
+            targets = random.sample(open_targets, 2)
+            # target1, target2 = targets_from_genome[targets[0][0]][targets[0][1]], targets_from_genome[targets[1][0]]targets[1][1]
+            print target1, target2
+            first = min(target1.current_start, target2.current_start)
+            second = max(target1.current_start, target2.current_start)
+            genome_camv.large_deletion(target1, target2)
+            turn_log += "Large deletion spanning from", first, "to", second + "\n"
 
     # cut and repair module
     for key_domain in targets_from_genome.keys():
@@ -102,7 +114,7 @@ for turn in xrange(total_turns):
         print turn_log
 
     # update plots if actively showing plots
-    if turn % plot_period == 0:
+    if turn % plot_period == 0 and make_plot:
         plot_path = os.path.join(plot_genome_folder, "genome_%05d" % plot_count)
         genome_plot_polar(genome_camv, 'CaMV', time=time_sim/60, output_path=plot_path, flag_show=False)
         plt.close()
